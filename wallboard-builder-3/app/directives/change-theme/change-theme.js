3bb66d4a331af1c93e64c0d42630853921bba3cc
danil <dvakhrushev@Danils-Air.bugfocus.com>
2017-09-07 15:13:06 -0700

    wallboard builder 3

---
+++ wallboard-builder-3/app/directives/change-theme/change-theme.js
@@ -0,0 +1,41 @@
+(function () {
+    //config for resize widget
+    var resizableConfig = {
+        autoHide: true,
+        distance: 5,
+        ghost: true,
+        //grid: [ 82, 54 ],
+        handles: "all",
+        disabled: true
+    };
+    //config for drag widget
+    var draggableConfig = {
+        cursor: 'move',
+        scroll: false,
+        disabled: true
+    };
+
+    angular.module('app')
+        .directive('changeTheme', function () {
+
+            return {
+                restrict: 'C',
+                scope: {
+                    wallboardId: '='
+                },
+                templateUrl: 'app/directives/change-theme/change-theme.html',
+
+                controller: ['$scope', 'StyleApi', 'WallboardStorage',
+                    function ($scope, StyleApi, WallboardStorage) {
+                        $scope.changeTheme = function(themeId){
+                            WallboardStorage.setTheme($scope.wallboardId, themeId);
+                        };
+
+                        $scope.dropTheme = function() {
+                            document.getElementById("myDropdown").classList.toggle("show");
+                        };
+                    }
+                ]
+            };
+        });
+})();
