81019af7e35c71e5cf36fbd9de302cd90a0d8d71
danil <dvakhrushev@Danils-Air.bugfocus.com>
2017-09-12 15:56:18 -0700

    fix wc1

--- wc1/js/constructor/helpers.js
+++ wc1/js/constructor/helpers.js
@@ -2,25 +2,19 @@ define(function (require, exports, module) {
     var configuration = require('snippet/configurationSnippet');
     var configurationPreview = require('constructor/configurationPreview');
     var configurationChat = require('client-chat-page/configurationChat');
-    var demoConf;
+
     var helpers = {
         listener: function (event) {
             console.log('GET MESSAGE3', event.data);
 
             if (helpers.isJsonString(event.data)) {
-                demoConf = JSON.parse(event.data);
+                var config = JSON.parse(event.data);
 
-                if (demoConf.screen) {
-                    var conf = {
-                        widgets: [{
-                            definition: demoConf.widget,
-                            styles: demoConf.styles
-                        }],
-                    };
+                if (config.screen) {
+                    $('#preview').attr('class', "").addClass(config.screen);
 
-                    $('#preview').attr('class', "").addClass(demoConf.screen);
-
-                    helpers.applyConfiguration(conf, 0);
+                    sessionStorage.setItem('confParams', JSON.stringify({widgets : [{ urls: [], definition: config.widget, styles: config.styles}]}));
+                    helpers.applyConfiguration();
                 }
             }
         },
